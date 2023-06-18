d9d75fd18894c1506fa8699b4077eab485ac2303
amakenzi <almaz.t97@gmail.com>
2023-06-19 04:44:12 +0500

    deploy

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var corsOptions = {
   origin: "http://localhost:3000",
 };
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
